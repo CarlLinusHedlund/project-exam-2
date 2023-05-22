47fd64789f7a48603471b4ca1e82c4b0793d787c
Carl Linus Hedlund <89521533+CarlLinusHedlund@users.noreply.github.com>
2023-05-22 09:50:36 +0200

    fixes swiper for venues nearby mobile

--- src/pages/home/components/VenuesNearby.jsx
+++ src/pages/home/components/VenuesNearby.jsx
@@ -1,12 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import "./index.css";
 
-import {
-  // setError,
-  // setLoading,
-  setLocation,
-} from "../../../store/modules/GeoloactionSlice";
+import { setLocation } from "../../../store/modules/GeoloactionSlice";
 import { client } from "../../../utils/PexelsClient";
 import { useDispatch, useSelector } from "react-redux";
 import { setImg, setQuery } from "../../../store/modules/PexelsSlice";
@@ -94,7 +90,7 @@ export default function VenuesNearby() {
               to={`/venues/country=${location.country}/type=apartment/city=${location.city}`}
               className="cursor-pointer group relative cols-span-2 row-span-1 row-start-1 col-start-4 col-end-6 rounded-[10px] hover:scale-[1.03] hover:shadow-2xl duration-200"
             >
-              <div className=" absolute w-full h-full top-0 left-0 bottom-0 right-0 duration-300 bg-[#00000017] group-hover:bg-[#0000002c] rounded-[10px] "></div>
+              <div className="absolute w-full h-full top-0 left-0 bottom-0 right-0 duration-300  group-hover:bg-[#0000002c] rounded-[10px]"></div>
               <div className=" absolute bottom-5 left-5 ">
                 <h2 className="duration-500 translate-x-5 group-hover:translate-x-0 text-primaryWhite opacity-0 group-hover:opacity-100 text-[16px] ">
                   In {location.city}
@@ -138,7 +134,7 @@ export default function VenuesNearby() {
                   In {location.city}
                 </h2>
                 <h3 className=" font-semibold leading-8 -translate-y-10 group-hover:translate-y-0 duration-500 text-primaryWhite  text-[25px] ">
-                  House
+                  Villas
                 </h3>
               </div>
               <img
@@ -160,20 +156,222 @@ export default function VenuesNearby() {
 }
 
 function VenuesNearbyMobile({ location, img }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  console.log("activeIndex", activeIndex);
+  const handleSlideChange = (swiper) => {
+    console.log("swiper", swiper);
+    setActiveIndex(swiper.realIndex);
+  };
   console.log("VenuesNearbyMobile", location);
   console.log("VenuesNearbyMobile", img);
 
   return (
     <Swiper
+      onSlideChange={handleSlideChange}
+      spaceBetween={10}
+      autoplay={{
+        delay: 2500,
+      }}
       loop={true}
       pagination={{
         clickable: true,
         dynamicBullets: true,
       }}
       modules={[Pagination]}
-      className="  mySwiper h-[50vh] min-h-[400px] w-full rounded-[10px]"
+      className={` ${
+        activeIndex === 0 || activeIndex === 2 ? "shadow-2xl" : ""
+      }  font-poppins relative mySwiper bg-primaryWhite h-[450px] w-full rounded-[10px] duration-200 `}
     >
-      <SwiperSlide></SwiperSlide>
+      <SwiperSlide className=" bg-primaryWhite flex overflow-hidden justify-center items-center text-[40px] rounded-[10px] w-full h-full">
+        <Link
+          className="relative w-full h-full bg-primaryWhite  "
+          to={`/venues/country=${location.country}`}
+        >
+          <div
+            className={` ${
+              activeIndex === 0 ? "bg-[#0000004a]" : " bg-none "
+            } absolute w-full h-full duration-1000 `}
+          ></div>
+          <div className="absolute bottom-20 left-4 text-primaryWhite flex flex-col gap-8">
+            <div>
+              <h3
+                className={` ${
+                  activeIndex === 0
+                    ? " opacity-100 translate-x-0 "
+                    : " translate-x-10 opacity-0"
+                } font-medium duration-700 delay-150 text-[16px] `}
+              >
+                Explore venues in
+              </h3>
+              <h2
+                className={` ${
+                  activeIndex === 0
+                    ? "opacity-100 translate-y-0"
+                    : "-translate-y-10 opacity-0"
+                } font-semibold leading-7 text-[40px] duration-500 delay-100`}
+              >
+                Norway
+              </h2>
+            </div>
+            <button
+              className={` ${
+                activeIndex === 0
+                  ? " opacity-100 scale-100 "
+                  : "opacity-0 scale-75"
+              } text-primaryDark font-medium rounded-[10px] text-[12px] xxs:text-[16px] px-16 xxs:px-20 py-2 bg-primaryWhite duration-500 delay-300 `}
+            >
+              Venues in {location.country}
+            </button>
+          </div>
+          <img
+            className=" w-full h-full object-cover rounded-[10px]"
+            src={img.src.src.large}
+            alt={`Photographer: ${img.src.photographer} text for image: ${img.src.alt} Link for image: ${img.src.url}`}
+          />
+        </Link>
+      </SwiperSlide>
+      <SwiperSlide className=" bg-primaryWhite flex flex-col gap-5 text-[40px] w-full h-full">
+        <Link
+          to={`/venues/country=${location.country}/type=apartment/city=${location.city}`}
+          className=" bg-primaryWhite relative h-1/2 w-full"
+        >
+          <div
+            className={` ${
+              activeIndex === 1 ? "bg-[#0000004a]" : " bg-none "
+            } absolute w-full h-full rounded-[10px] duration-1000 `}
+          ></div>
+          <div className="absolute bottom-10 left-4 text-primaryWhite flex flex-col gap-8">
+            <div>
+              <h3
+                className={` ${
+                  activeIndex === 1
+                    ? " opacity-100 translate-x-0 "
+                    : " translate-x-10 opacity-0"
+                } font-semibold delay-300 duration-700 text-[18px] `}
+              >
+                In {location.city}
+              </h3>
+              <h2
+                className={` ${
+                  activeIndex === 1
+                    ? "opacity-100 translate-y-0"
+                    : "-translate-y-10 opacity-0"
+                } font-semibold leading-7 text-[30px] duration-500 delay-100`}
+              >
+                Apartments
+              </h2>
+            </div>
+            {/* <button
+              className={` ${
+                activeIndex === 1
+                  ? " opacity-100 scale-100 "
+                  : "opacity-0 scale-75"
+              } text-primaryDark font-medium rounded-[10px] text-[12px] xxs:text-[16px] px-14 xxs:px-18 py-2 bg-primaryWhite duration-500 delay-300 `}
+            >
+              Apartments in {location.city}
+            </button> */}
+          </div>
+          <img
+            className=" w-full h-full object-cover rounded-[10px]"
+            src="../apartmentImg.jpg"
+            alt={`Photographer: ${img.src.photographer} text for image: ${img.src.alt} Link for image: ${img.src.url}`}
+          />
+        </Link>
+        <Link
+          className="relative bg-primaryWhite h-1/2 w-full"
+          to={`/venues/country=${location.country}/type=hotel/city=${location.city}`}
+        >
+          <div
+            className={` ${
+              activeIndex === 1 ? "bg-[#0000004a]" : " bg-none "
+            } absolute w-full rounded-[10px] h-full duration-1000 `}
+          ></div>
+          <div className="absolute bottom-10 left-4 text-primaryWhite flex flex-col gap-8">
+            <div>
+              <h3
+                className={` ${
+                  activeIndex === 1
+                    ? " opacity-100 translate-x-0 "
+                    : " translate-x-10 opacity-0"
+                } font-semibold delay-300 duration-700 text-[18px] `}
+              >
+                In {location.city}
+              </h3>
+              <h2
+                className={` ${
+                  activeIndex === 1
+                    ? "opacity-100 translate-y-0"
+                    : "-translate-y-10 opacity-0"
+                } font-semibold leading-7 text-[30px] duration-500 delay-100`}
+              >
+                Hotels
+              </h2>
+            </div>
+            {/* <button
+              className={` ${
+                activeIndex === 1
+                  ? " opacity-100 scale-100 "
+                  : "opacity-0 scale-75"
+              } text-primaryDark font-medium rounded-[10px] text-[12px] xxs:text-[16px] px-14 xxs:px-18 py-2 bg-primaryWhite duration-500 delay-300 `}
+            >
+              Apartments in {location.city}
+            </button> */}
+          </div>
+          <img
+            className=" w-full h-full object-cover rounded-[10px]"
+            src="../hotelRoom.jpg"
+            alt={`Photographer: ${img.src.photographer} text for image: ${img.src.alt} Link for image: ${img.src.url}`}
+          />
+        </Link>
+      </SwiperSlide>
+      <SwiperSlide className=" flex rounded-[10px]  justify-center items-center text-[40px] w-full h-full">
+        <Link
+          className="reltive h-full w-full "
+          to={`/venues/country=${location.country}/type=house/city=${location.city}`}
+        >
+          <div
+            className={` ${
+              activeIndex === 2 ? "bg-[#0000004a]" : " bg-none "
+            } absolute w-full h-full duration-1000 `}
+          ></div>
+          <div className="absolute bottom-20 left-4 text-primaryWhite flex flex-col gap-8">
+            <div>
+              <h3
+                className={` ${
+                  activeIndex === 2
+                    ? " opacity-100 translate-x-0 "
+                    : " translate-x-10 opacity-0"
+                } font-semibold delay-300 duration-700 text-[18px] `}
+              >
+                In {location.city}
+              </h3>
+              <h2
+                className={` ${
+                  activeIndex === 2
+                    ? "opacity-100 translate-y-0"
+                    : "-translate-y-10 opacity-0"
+                } font-semibold leading-7 text-[40px] duration-500 delay-100`}
+              >
+                Villas
+              </h2>
+            </div>
+            <button
+              className={` ${
+                activeIndex === 2
+                  ? " opacity-100 scale-100 "
+                  : "opacity-0 scale-75"
+              } text-primaryDark font-medium rounded-[10px] text-[12px] xxs:text-[16px] px-16 xxs:px-20 py-2 bg-primaryWhite duration-500 delay-300 `}
+            >
+              House in {location.city}
+            </button>
+          </div>
+          <img
+            className=" w-full h-full object-cover rounded-[10px]"
+            src="../house.jpg"
+            alt={`Photographer: ${img.src.photographer} text for image: ${img.src.alt} Link for image: ${img.src.url}`}
+          />
+        </Link>
+      </SwiperSlide>
     </Swiper>
   );
 }
